0602e6b3bb1747175bb23f338f4ee4cc1e9b9cee
SuryaaDev <suryaa62917@gmail.com>
2025-11-11 16:56:33 +0700

    update 2.1 api

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://nilai.vortech.my.id/api",
 });
 
 API.interceptors.request.use((config) => {
